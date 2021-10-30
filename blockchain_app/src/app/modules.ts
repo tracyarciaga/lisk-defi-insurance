1a5a19eaf891e0a0483903bb6b813d1b84fe3aed
tracy <teresa.arciaga@gmail.com>
2021-10-30 10:45:41 +0800

    add insurance module and nvmrc

--- blockchain_app/src/app/modules.ts
+++ blockchain_app/src/app/modules.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { InsuranceModule } from "./modules/insurance/insurance_module";
 
 // @ts-expect-error Unused variable error happens here until at least one module is registered
-export const registerModules = (_app: Application): void => {};
+export const registerModules = (_app: Application): void => {
+
+    _app.registerModule(InsuranceModule);
+};
